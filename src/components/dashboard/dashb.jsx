fbf3001440a4c9002b247eacff49ccc2f0b1945a
gostlite <foodubnig@gmail.com>
2023-05-03 16:43:00 -0700

    dash update

--- src/components/dashboard/dashb.jsx
+++ src/components/dashboard/dashb.jsx
@@ -7,11 +7,6 @@ export default function Dashboard() {
     return <div className="dashboard-container">
   <div className="sidebar">
     <h2>Dashboard</h2>
-    {/* <ul>
-      <li><a href="#">Overview</a></li>
-      <li><a href="#">Reports</a></li>
-      <li><a href="#">Settings</a></li>
-    </ul> */}
   </div>
   <div className="main-content">
     <h2>Overview</h2>
